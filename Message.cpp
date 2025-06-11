71ade28aa756bd079a194c31d2f590ec3cb0e8ad
MihailVK04 <149954001+MihailVK04@users.noreply.github.com>
2025-06-11 22:23:05 +0300

    Add files via upload
    Added Message class to manage messages in the application

---
+++ Message.cpp
@@ -0,0 +1,47 @@
+#include "Message.h"
+
+Message::Message() {
+	sender = "Unknown";
+	time = std::time(nullptr);
+}
+
+Message::Message(const Sentences& _message, const String& _sender) : message(_message), sender(_sender) {
+	time = std::time(nullptr);
+}
+
+String Message::getSender() const
+{
+	return sender;
+}
+
+Sentences Message::getMessage() const
+{
+	return message;
+}
+
+time_t Message::getTime() const
+{
+	return time;
+}
+
+void Message::saveToFile(std::ofstream& os) {
+	os.write((const char*)&time, sizeof(time_t));
+	this->sender.saveToFile(os);
+	this->message.saveToFile(os);
+}
+
+void Message::readFromFile(std::ifstream& is)
+{
+	is.read((char*)&time, sizeof(time_t));
+	this->sender.readFromFile(is);
+	this->message.readFromFile(is);
+}
+
+std::ostream& operator<<(std::ostream& os, Message& ref)
+{
+	os << "--------" << '\n';
+	os << ref.sender << '\n';
+	os << ref.message << '\n';
+	os << "Sent: " << std::ctime(&ref.time) << '\n';
+	return os;
+}
---
+++ Message.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "String.h"
+#include "Sentences.h"
+#include <ctime>
+#pragma warning (disable:4996)
+
+class Message {
+private:
+	String sender;
+	Sentences message;
+	time_t time;
+
+public:
+	Message();
+	Message(const Sentences& _message, const String& _sender);
+	String getSender() const;
+	Sentences getMessage() const;
+	time_t getTime() const;
+	friend std::ostream& operator<<(std::ostream& os, Message& ref);
+	void saveToFile(std::ofstream& os);
+	void readFromFile(std::ifstream& is);
+};
